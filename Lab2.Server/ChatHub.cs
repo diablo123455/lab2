14945530f3505ac94eec0f1f72b76a1dae33c73d
Влад Гуменный <vg@paraweb.me>
2019-11-30 09:37:32 +0700

    Initial commit

---
+++ Lab2.Server/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Lab2.Server
+{
+    public class ChatHub : Hub
+    {
+        public void SendToAll(string name, string message)
+        {
+            Clients.All.InvokeAsync("sendToAll", name, message);
+        }
+    }
+}
